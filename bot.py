735d2533a5cc0ee5e12d11a3e725c3029ecf6d19
ChilliVillli <lionivanov777@gmail.com>
2024-06-19 19:45:50 +0300

    For checking!

--- bot.py
+++ bot.py
@@ -11,9 +11,20 @@ from loader import scheduler
 
 
 list_num = []
-list_new = []
 ua = UserAgent()
 router = Router()
+list_cat = [
+        'ordinary,bronze,bronze_vip,bronze AAA', 'platinum,platinum_lite', 'brilliant,brilliant_super',
+        'gold', 'silver,silver_special,silver_special_2'
+    ]
+dict_cat = {
+    'gold': 'gold',
+    'brilliant': 'brilliant,brilliant_super',
+    'platinum': 'platinum,platinum_lite',
+    'silver': 'silver,silver_special,silver_special_2',
+    'bronze': 'ordinary,bronze,bronze_vip,bronze+AAA',
+    '': ''
+}
 
 
 class FiltresSearch(StatesGroup):
@@ -33,7 +44,6 @@ async def comand_start_handler(message: Message):
 async def cmd_cancel(message: Message, state: FSMContext):
 
     await state.clear()
-    flag = False
     await message.answer('Поиск отменен', reply_markup=menu_admin)
 
 
@@ -80,41 +90,43 @@ async def cat_num(message: Message, state: FSMContext):
     tarif_phone = data['tarif']
     maska_phone = data['maska_num']
     cat_phone = data['categories']
-    dict_cat = {
-        'gold': 'gold',
-        'brilliant': 'brilliant,brilliant_super',
-        'platinum': 'platinum,platinum_lite',
-        'silver': 'silver,silver_special,silver_special_2',
-        'bronze': 'ordinary,bronze,bronze_vip,bronze+AAA',
-        '': ''
-    }
     cat_phone = dict_cat[cat_phone]
     await message.answer("Приступаю к поиску!", reply_markup=cancel)
-    await base_set(tarif_phone, maska_phone, cat_phone)
+    await base_set(tarif_phone, maska_phone, cat_phone, list_cat)
     await search(message, tarif_phone, maska_phone, cat_phone)
     scheduler.add_job(search, "interval", seconds=2, args=(message, tarif_phone, maska_phone, cat_phone))
 
 
-async def base_set(tarif_phone, maska_phone, cat_phone):
+async def base_set(tarif_phone, maska_phone, cat_phone, list_cat):
 
     headers = {'User-agent': ua.random}
     session = requests.Session()
     session.headers.update(headers)
 
     res_num = session.get(
-        f"https://api.store.bezlimit.ru/v2/super-link/phones/mask-category?mask-category={cat_phone}&service_limit={tarif_phone}&"
-        f"group_by=mask-category&expand=tariff,region&per_page=200")  # bezlimit
+        f"https://api.store.bezlimit.ru/v2/super-link/phones/mask-category?mask-category={cat_phone}"
+        f"&phone_pattern={maska_phone}&service_limit={tarif_phone}&group_by=mask-category&expand=tariff,region&per_page=500"
+    )  # bezlimit
 
     dict_num = res_num.json()
-    # print(dict_num) # ['ordinary,bronze,bronze_vip,bronze AAA'] ['platinum,platinum_lite'] ['brilliant,brilliant_super']
 
-    for i in dict_num[cat_phone]['items']:
+    if cat_phone not in list_cat:
 
-        if i['phone'] not in list_num:
-            list_num.append(i['phone'])
-        else:
-            continue
-    print(list_num)
+        for j in list_cat:
+            if len(dict_num[j]['items']) == 0:
+                continue
+            else:
+                for i in dict_num[j]['items']:
+                    if i['phone'] not in list_num:
+                        list_num.append(i['phone'])
+                    else:
+                        continue
+
+    if cat_phone in list_cat:
+        for i in dict_num[cat_phone]['items']:
+            if i['phone'] not in list_num:
+                list_num.append(i['phone'])
+
     return dict_num
 
 
@@ -124,34 +136,14 @@ async def search(message, tarif_phone, maska_phone, cat_phone):
     session = requests.Session()
     session.headers.update(headers)
 
-    res_num = session.get(f"https://api.store.bezlimit.ru/v2/super-link/phones/mask-category?service_limit={tarif_phone}&"
-                            f"group_by=mask-category&expand=tariff,region&per_page=200") #bezlimit
-
-    # data_num = {
-    #     'sort': '-tariff_price',
-    #     'type': 'standard',
-    #     'service_limit': tarif_phone,
-    #     'is_reserved': 'true',
-    #     'per_page': 6,
-    #     'expand': 'reservation,tariff,region,mask,priceParams'
-    # }
-
-    # res_num = session.get(f"https://api.store.bezlimit.ru/v2/phones?sort=-tariff_price&type=standard&"
-    #                       f"service_limit={tarif_phone}&is_reserved=true&per_page=6&expand=reservation,tariff,region,mask,priceParams") #bezlimit_store
-    # res_num = session.get(
-    #     f'https://www.anncom.ru/dialer/lendingbezlimit/main.py?tariff={tarif_phone}&'
-    #     f'phone_pattern={maska_phone}&region=&pattern=categories&phone_categories={cat_phone}&pageLimit=1000'
-    # ) #bezlimit_club
+    res_num = session.get(
+        f"https://api.store.bezlimit.ru/v2/super-link/phones/mask-category?mask-category={cat_phone}&"
+        f"phone_pattern={maska_phone}&service_limit={tarif_phone}&group_by=mask-category&expand=tariff,region&per_page=1500"
+    ) #bezlimit
 
     dict_num = res_num.json()
-    # print(dict_num) # ['ordinary,bronze,bronze_vip,bronze AAA'] ['platinum,platinum_lite'] ['brilliant,brilliant_super']
 
-    # for i in dict_num['platinum,platinum_lite']['items']:
-    #
-    #     if i['phone'] not in list_num:
-    #         list_num.append(i['phone'])
-    #     else:
-    #         continue
+    """reservetion number"""
         # data_num = {'phone': i['phone'],
         #             'tariff_id': '13101',
         #             'type': 'store',
@@ -162,17 +154,39 @@ async def search(message, tarif_phone, maska_phone, cat_phone):
         # reservetion = session.options(
         #     f"https://api.store.bezlimit.ru/v2/super-link/reservations?phone={i['phone']}&user_id=486739") #480524
         # await message.answer(f"Номер - {i['phone']} забронирован!")
+    return await publication(message, dict_num, cat_phone, list_cat)
 
-    return await publication(message, dict_num)
 
+async def publication(message, dict_num, cat_phone, list_cat):
 
-async def publication(message, dict_num):
+    if cat_phone not in list_cat:
 
-    for i in dict_num['platinum,platinum_lite']['items']:
+        for j in list_cat:
+            if len(dict_num[j]['items']) == 0:
+                continue
+            else:
+                for i in dict_num[j]['items']:
+                    if i['phone'] not in list_num:
+                        list_num.append(i['phone'])
+                        await message.answer(f"{i['phone']}")
+                        await asyncio.sleep(1)
+                    else:
+                        continue
 
-        if i['phone'] not in list_num:
-            list_num.append(i['phone'])
-            await message.answer(f"{i['phone']}")
-            continue
+    if cat_phone in list_cat:
+        for i in dict_num[cat_phone]['items']:
+            if i['phone'] not in list_num:
+                list_num.append(i['phone'])
+                await message.answer(f"{i['phone']}")
+                await asyncio.sleep(1)
+            else:
+                continue
 
-        await asyncio.sleep(1)
+    # for i in dict_num['platinum,platinum_lite']['items']:
+    #
+    #     if i['phone'] not in list_num:
+    #         list_num.append(i['phone'])
+    #         await message.answer(f"{i['phone']}")
+    #         continue
+    #
+    #     await asyncio.sleep(1)
